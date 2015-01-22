8867e69828f8e6ad5498e3f2a2cf4acb88b35acf
Zaicheng Qi <vmlinz@gmail.com>
2015-01-22 10:04:16 +0000

    Add static pages

--- app.rb
+++ app.rb
@@ -1,10 +1,16 @@
 require 'sinatra'
 require 'sinatra/base'
 
+set :public_folder, Proc.new { File.join(root, "static") }
+
 class App < Sinatra::Base
   get '/' do
     'Hello from docker!'
   end
-end
 
+  get '/hello/' do
+    greeting = params[:greeting] || "Hi, there"
+    erb :index, :locals => {'greeting' => greeting}
+  end
+end
 
